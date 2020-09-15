131bb0ef5604cde343d0336c643095ff1be2e6c4
Daniel He <danielhe.mlp@gmail.com>
2020-09-15 18:03:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,13 @@ def line(katz_deli)
   #check line empty
   if(katz_deli.length == 0)
     puts "The line is currently empty."
-    return
-  end
-
   #show everyones current place in line
-  counter=1
-  output = "The line is currently: "
-  katz_deli.each {|a| output += (counter.to_s+". #{a} "); counter+=1}
-  puts output.strip
+  else
+    counter=1
+    output = "The line is currently:"
+    katz_deli.each {|name| output += (" #{counter.to_s}. #{name}"); counter+=1}
+    puts output
+  end
 end
 
 def take_a_number(katz_deli,name)
@@ -21,9 +20,8 @@ def now_serving(katz_deli)
   #check line empty
   if(katz_deli.length == 0)
     puts "There is nobody waiting to be served!"
-    return
-  end
-
   #serve next in line
-  puts "Currently serving #{katz_deli.shift}."
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
 end
